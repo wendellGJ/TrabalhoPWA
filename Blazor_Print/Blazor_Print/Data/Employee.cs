d67dcb49079b6eba800cf51cbebef96277bff96d
Wendell <junior.wendell@hotmail.com>
2023-06-23 19:25:15 -0300

    feat: add trabalho

---
+++ Blazor_Print/Blazor_Print/Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor_Print.Data
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Blazor_Print/Blazor_Print/Data/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Blazor_Print.Data
+{
+    public class EmployeeService
+    {
+        private readonly AppDbContext _context;
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+    }
+}
